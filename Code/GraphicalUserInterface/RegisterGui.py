0d75f236a0d2d2023cc05a89606d348e11162782
issolis <issolis@estudiantec.cr>
2023-10-01 23:59:43 -0600

    Registro de canción favorita

--- Code/GraphicalUserInterface/RegisterGui.py
+++ Code/GraphicalUserInterface/RegisterGui.py
@@ -1,7 +1,10 @@
 from tkinter  import *
 from tkinter import messagebox
+from tkinter import ttk
+import tkinter as tk
 from registerGUIAnswers import registerGUIAnswers
 from User import *
+from musicLogic import *
 
 class registerGui: 
 
@@ -11,7 +14,8 @@ class registerGui:
         self.width = width
         self.height = height
         self.parentFrame=parentFrame
-        
+        self.songs=[]
+        self.controler=musicLogic()
 
         centerX  = width/2
         centrerY = width/2
@@ -29,25 +33,51 @@ class registerGui:
         self.colorVar.set("default")
 
         # Crear radio button para el color Rojo
-        Radiobutton(self.InformationFrame, text="Rojo     ", variable=self.colorVar, font=(font,15), width=8, value="red", command=self.showColor).place(x=centerX+300, y=250, anchor="center")
+        Radiobutton(self.InformationFrame, text="Rojo     ", variable=self.colorVar, font=(font,15), width=8, value="red", command=self.showColor).place(x=centerX+330, y=250, anchor="center")
 
         # Crear radio button para el color Verde
-        Radiobutton(self.InformationFrame, text="Verde   ", variable=self.colorVar, font=(font,15), width=8, value="green", command=self.showColor).place(x=centerX+300, y=300, anchor="center")
+        Radiobutton(self.InformationFrame, text="Verde   ", variable=self.colorVar, font=(font,15), width=8, value="green", command=self.showColor).place(x=centerX+330, y=300, anchor="center")
 
         # Crear radio button para el color Azul
-        Radiobutton(self.InformationFrame, text="Azul     ", variable=self.colorVar, font=(font,15), width=8,value="blue", command=self.showColor).place(x=centerX+300, y=350, anchor="center")
+        Radiobutton(self.InformationFrame, text="Azul     ", variable=self.colorVar, font=(font,15), width=8,value="blue", command=self.showColor).place(x=centerX+330, y=350, anchor="center")
 
         # Crear radio button para el color Amarillo
-        Radiobutton(self.InformationFrame, text="Amarillo", variable=self.colorVar, font=(font,15), width=8, value="yellow", command=self.showColor).place(x=centerX+300, y=400, anchor="center")
+        Radiobutton(self.InformationFrame, text="Amarillo", variable=self.colorVar, font=(font,15), width=8, value="yellow", command=self.showColor).place(x=centerX+330, y=400, anchor="center")
 
         # Crear radio button para el color naranja
-        Radiobutton(self.InformationFrame, text="Naranja", variable=self.colorVar, font=(font,15), width=8, value="orange", command=self.showColor).place(x=centerX+300, y=450, anchor="center")
+        Radiobutton(self.InformationFrame, text="Naranja", variable=self.colorVar, font=(font,15), width=8, value="orange", command=self.showColor).place(x=centerX+330, y=450, anchor="center")
 
         # Crear radio button para el color naranja
-        Radiobutton(self.InformationFrame, text="Morado", variable=self.colorVar, font=(font,15), width=8, value="violet", command=self.showColor).place(x=centerX+300, y=500, anchor="center")
+        Radiobutton(self.InformationFrame, text="Morado", variable=self.colorVar, font=(font,15), width=8, value="violet", command=self.showColor).place(x=centerX+330, y=500, anchor="center")
 
         self.colorLabel = Label(self.InformationFrame, text="Seleccione su color favorito", font=(font, 15))
-        self.colorLabel.place(x=centerX+300, y=200, anchor="center")
+        self.colorLabel.place(x=centerX+330, y=200, anchor="center")
+
+        ##
+
+        ##
+        self.songLabel = Label(self.InformationFrame, text="Seleccione su color favorito", font=(font, 15))
+        self.songLabel.place(x=centerX+10, y=200, anchor="center")
+
+        self.chosenSong = tk.StringVar()
+
+        # Crear el ComboBox
+        self.songOptions = ttk.Combobox(root, textvariable=self.chosenSong, height= 40, width=40, values=[])
+        self.songOptions.place(x=centerX+10, y=300, anchor="center")
+
+        self.songLabel = Label(self.InformationFrame, text="Ingrese el nombre de la canción", font=(font, 15))
+        self.songLabel.place(x=centerX+10, y=200, anchor="center")
+
+        self.nameSongEntry= Entry(self.InformationFrame,width=18, font=(font, 15))
+        self.nameSongEntry.place(x=centerX-20, y=250, anchor="center")
+
+        self.searchBtn = Button(self.InformationFrame, text="Search", font=(font, 10), command = self.searchSongs)
+        self.searchBtn.place(x=centerX+88, y=238, anchor="nw") 
+        
+        self.saveBtn = Button(self.InformationFrame, text="Esperando...", font=(font, 10), command = self.saveSong)
+        self.saveBtn.place(x=centerX-30, y=330, anchor="nw")
+        self.saveBtn.config(state="disabled")
+
 
         ##
 
@@ -58,24 +88,24 @@ class registerGui:
         self.registerLb.place(x=centerX, y=125, anchor="center")
 
         self.questionOneLb= Label (self.InformationFrame, text="Ingrese su nombre de usuario", font=(font,15))
-        self.questionOneLb.place(x=centerX-300, y=200, anchor="center")
+        self.questionOneLb.place(x=centerX-330, y=200, anchor="center")
         self.questionOneEntry= Entry(self.InformationFrame,width=25, font=(font, 15))
-        self.questionOneEntry.place(x=centerX-300, y=250, anchor="center")
+        self.questionOneEntry.place(x=centerX-330, y=250, anchor="center")
 
         self.questionTwoLb= Label (self.InformationFrame, text="Ingrese su correo", font=(font,15))
-        self.questionTwoLb.place(x=centerX-300, y=300, anchor="center")
+        self.questionTwoLb.place(x=centerX-330, y=300, anchor="center")
         self.questionTwoEntry= Entry(self.InformationFrame,width=25, font=(font, 15))
-        self.questionTwoEntry.place(x=centerX-300, y=350, anchor="center")
+        self.questionTwoEntry.place(x=centerX-330, y=350, anchor="center")
 
         self.questionThreeLb= Label (self.InformationFrame, text="Ingrese su contraseña", font=(font,15))
-        self.questionThreeLb.place(x=centerX-300, y=400, anchor="center")
+        self.questionThreeLb.place(x=centerX-330, y=400, anchor="center")
         self.questionThreeEntry= Entry(self.InformationFrame,width=25, font=(font, 15))
-        self.questionThreeEntry.place(x=centerX-300, y=450, anchor="center")
+        self.questionThreeEntry.place(x=centerX-330, y=450, anchor="center")
 
         self.questionFourLb= Label (self.InformationFrame, text="Confirme su contraseña", font=(font,15))
-        self.questionFourLb.place(x=centerX-300, y=500, anchor="center")
+        self.questionFourLb.place(x=centerX-330, y=500, anchor="center")
         self.questionFourEntry= Entry(self.InformationFrame,width=25, font=(font, 15))
-        self.questionFourEntry.place(x=centerX-300, y=550, anchor="center")
+        self.questionFourEntry.place(x=centerX-330, y=550, anchor="center")
 
         self.nextBtn = Button(self.InformationFrame, text="next", font=(font, 15), command = self.nextPage)
         self.nextBtn.place(x=2*centerX-100, y=650, anchor="nw")
@@ -90,8 +120,8 @@ class registerGui:
                         user=User(self.questionOneEntry.get(),
                                 self.questionThreeEntry.get(),
                                 self.questionTwoEntry.get(),
-                                self.colorVar.get(),"",
-                                [""]," "," ")
+                                self.colorVar.get(),
+                                self.songs," "," ","")
                         if(user.validation):
                             self.InformationFrame.pack_forget()
                             app=registerGUIAnswers(self.window,self.width,self.height,user,self.parentFrame)
@@ -132,3 +162,49 @@ class registerGui:
     def showColor(self):
         color = self.colorVar.get()
         self.colorLabel.config(bg=color)
+        
+
+    def searchSongs(self): 
+        
+        self.saveBtn.config(text="Cargando...")
+        self.saveBtn.update()
+        self.controler.searchYoutubeSongs(self.nameSongEntry.get())
+        self.songOptions['values']=self.controler.nameSongListForUser
+        self.saveBtn.config(text="Guardar")
+        self.saveBtn.config(state="normal")
+        self.chosenSong.set(self.controler.nameSongListForUser[0])
+        self.saveBtn.update()
+
+    def saveSong(self): 
+        counter=0
+        self.saveBtn.config(state="disabled")
+        self.songOptions['values']=[]
+        
+        while counter!= len(self.controler.nameSongListForDev):
+           
+            if self.controler.nameSongListForUser[counter]==self.chosenSong.get():
+                self.songs.append(self.controler.nameSongListForDev[counter])
+                break
+            counter+=1
+        self.chosenSong.set("")
+
+
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    # take the dimensions of the computer screen
+    widthScreen = root.winfo_screenwidth()
+    heightScreen = root.winfo_screenheight()
+
+    # set game screen size
+    root.geometry(f"{widthScreen}x{heightScreen}")
+    root.resizable(False, False)
+
+    root.title("Eagle Defender")
+
+    app =registerGui(root, widthScreen, heightScreen,NONE)
+
+    root.mainloop()
+
