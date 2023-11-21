6f93a7a50cb6eed517f856a9c0f79b75eb0106ac
Frederick <fredeoscawits5@gmail.com>
2023-11-20 21:14:14 -0600

    First refactor commit

--- Code/GraphicalUserInterface/helpSection.py
+++ Code/GraphicalUserInterface/helpSection.py
@@ -15,10 +15,10 @@ class HelpSection:
 
         colorPalette = ["#8B0000", "#630000", "#1C1C1C", "#000000", "#FFFFFF"]
 
-        self.attackerControls = Image.open("Code/GraphicalUserInterface/GameData/Control-Attacker2.png").resize((780,700))
+        self.attackerControls = Image.open("Code/GraphicalUserInterface/GameData/Control-Attacker.png").resize((700,650))
         self.attackerControls = ImageTk.PhotoImage(self.attackerControls)
 
-        self.defenderControls = Image.open("Code/GraphicalUserInterface/GameData/Control-Defender2.png").resize((780,700))
+        self.defenderControls = Image.open("Code/GraphicalUserInterface/GameData/Control-Defender.png").resize((700,650))
         self.defenderControls = ImageTk.PhotoImage(self.defenderControls)
 
         # frames are used to facilitate the creation and deletion of screens
@@ -27,20 +27,17 @@ class HelpSection:
 
         # is replaced by the logo
         # Lb: Label. Specify the type of element
-        self.titleCanvas = tk.Canvas(self.initialFrame, width=self.width, height=self.height, bg=colorPalette[1])
-        self.titleCanvas.pack()
-        self.titleCanvas.config(borderwidth=0, highlightthickness=0)
+        self.Canvas = tk.Canvas(self.initialFrame, width=self.width, height=self.height, bg=colorPalette[1])
+        self.Canvas.pack()
+        self.Canvas.config(borderwidth=0, highlightthickness=0)
 
-        self.titleLb = tk.Label(self.titleCanvas, text="Controles de juego", font=(font, 50))
-        self.titleLb.config(bg=colorPalette[1], fg=colorPalette[3])
-        self.titleLb.place(x=centerX, y=100, anchor="center")
-
-        self.returnBtn = tk.Button(self.titleCanvas, text="Retornar al menu", command=self.backToMenu )
+        self.returnBtn = tk.Button(self.Canvas, text="Retornar al menu", command=self.backToMenu )
         self.returnBtn.place(x=self.width/2-50, y=self.height-75)
         
         # Create images on the canvas
-        self.titleCanvas.create_image(self.width//2+530, self.height//2-75, image=self.attackerControls) # (Attacker controls)
-        self.titleCanvas.create_image(self.width//2-530, self.height//2-75, image=self.defenderControls) # (Defender controls)
+        self.Canvas.create_image(self.width//2+530, self.height//2-75, image=self.attackerControls) # (Attacker controls)
+        self.Canvas.create_image(self.width//2-530, self.height//2-75, image=self.defenderControls) # (Defender controls)
+        self.Canvas.create_text(centerX, 60, text="Controles de juego", font=(font, 50))
     
     def backToMenu(self):
         self.initialFrame.pack_forget()
